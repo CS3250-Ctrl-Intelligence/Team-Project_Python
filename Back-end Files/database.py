7add5cca25382c044ccaac378dd44e31e6b5d0da
SHamil36 <seth.hamilton@aol.com>
2022-03-04 18:20:09 -0700

    added the folder to store all the backend files so we can seperate front end and back end

---
+++ Back-end Files/database.py
@@ -0,0 +1,79 @@
+import mysql.connector as mc
+
+#function to create the database if it does not exist
+def createDB():
+
+    mydb = mc.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS project")
+
+#function to create the table inventory in the project database if it does not exist.
+def createInventory():
+
+    mydb = mc.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS inventory (Product_ID VARCHAR(45), Quantity INT, Wholesale_Price FLOAT, Sale_Price FLOAT, Supplier_ID VARCHAR(45))")
+
+#function to create the table orders in the project database if it does not exist.
+def createOrders():
+    
+    mydb = mc.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS orders (Date DATE, Cust_Email VARCHAR(45), Cust_Location INT, Product_ID VARCHAR(45), Quantity INT)")
+
+#function to create the table employees in the project database if it does not exist.
+def createEmployees():
+    
+    mydb = mc.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS employees (username VARCHAR(45), password VARCHAR(45))")
+
+#function to create the table customers in the project database if it does not exist.
+def createCustomers():
+    
+    mydb = mc.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="project"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS customers (cust_email VARCHAR(45), username VARCHAR(45), password VARCHAR(45))")
+
+#main function to call all previous functions into main.py
+def main():
+    createDB()
+    createInventory()
+    createOrders()
+    createEmployees()
+    createCustomers()
+
+main()
