1746dff544ac728c36d92e3d848753471ae47e9a
Matthewjww <Matthewjww@gmail.com>
2022-03-07 19:32:05 -0700

    Added unit tests to database.py

--- backend_files/database.py
+++ backend_files/database.py
@@ -1,4 +1,5 @@
 import mysql.connector as mc
+import unittest
 
 #function to create the database if it does not exist
 def createDB():
@@ -77,3 +78,51 @@ def main():
     createCustomers()
 
 main()
+
+
+class TestDatabase(unittest.TestCase):
+
+    def test_write(self):
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+        )
+        test_cursor = mydb.cursor()
+
+        test_cursor.execute("CREATE DATABASE IF NOT EXISTS unittest")
+
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="unittest"
+        )
+        test_cursor = mydb.cursor()
+
+        test_cursor.execute("CREATE TABLE IF NOT EXISTS test_table (id INT, text VARCHAR(45))")
+
+        test_cursor.execute("INSERT INTO `test_table` (id, `text`) VALUES (3, 'test_text_3')")
+
+        test_cursor.execute("SELECT text FROM test_table ORDER BY id DESC LIMIT 1")
+
+        test = test_cursor.fetchall()
+
+        self.assertEqual(test, [('test_text_3',)])
+
+    def test_delete(self):
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="unittest"
+        )
+        test_cursor = mydb.cursor()
+
+        test_cursor.execute("""DELETE FROM `test_table` WHERE id= 3 """)
+
+        test_cursor.execute("""SELECT text FROM test_table ORDER BY id DESC LIMIT 1""")
+
+        test = test_cursor.fetchall()
+
+        self.assertEqual(test, [])
