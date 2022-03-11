75cc5cfeb3649a1860a45def5b5b0edee7b68bd0
Brian Vasquez <Brian_07@live.com>
2022-03-10 17:46:16 -0700

    Implemented the cart, checkout and contact us pages

--- frontend_files/ci_project/ci_app/models/customer.py
+++ frontend_files/ci_project/ci_app/models/customer.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, null= True, blank =True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null= True, blank =True)
     name = models.CharField(max_length=126, null = True)
     email = models.CharField(max_length=126, null = True)
 
